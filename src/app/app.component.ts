275bbfc41604b889d8b282bde380c64476fab5ae
Andrei Voicu <Andrei-Florian.Voicu@haufe-lexware.com>
2017-10-02 14:29:50 +0300

    trying again, empty project.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,29 +1,14 @@
-import { Component, OnDestroy, OnInit, Renderer2 } from '@angular/core';
-import { WindowScrollSpyService } from './services/window-scroll-spy/window-scroll-spy.service';
-import { ReplaySubject } from 'rxjs/ReplaySubject';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+    <p>
+      app Works!
+    </p>
+  `,
+  styles: []
 })
-export class AppComponent implements OnInit, OnDestroy {
-  private windowScrollStream$: ReplaySubject<any> = new ReplaySubject(1);
-  private removeWindowListener: Function;
-
-  constructor(private scrollSpy: WindowScrollSpyService,
-              private renderer: Renderer2) {
-  }
-
-  ngOnInit(): void {
-    this.scrollSpy.setWindowScrollStream(this.windowScrollStream$);
-
-    this.removeWindowListener = this.renderer.listen('window', 'scroll', ($event) => {
-      this.windowScrollStream$.next($event);
-    });
-  }
-
-  ngOnDestroy(): void {
-    this.removeWindowListener();
-  }
+export class AppComponent {
+  title = 'app';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
-import { WindowScrollSpyService } from './services/window-scroll-spy/window-scroll-spy.service';
 
 @NgModule({
-    declarations: [AppComponent],
-    imports: [
-        BrowserModule,
-        FormsModule,
-        HttpModule
-    ],
-    providers: [WindowScrollSpyService],
-    bootstrap: [AppComponent]
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
 })
-export class AppModule {
-}
+export class AppModule { }
