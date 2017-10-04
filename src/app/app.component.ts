da75777a611306ab049211cdefa8aceb4779eb7b
Andrei Voicu <Andrei-Florian.Voicu@haufe-lexware.com>
2017-10-04 13:44:02 +0300

    made the header height adaptive.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,30 +1,46 @@
 import {AfterViewInit, Component, ElementRef, HostBinding, OnDestroy, OnInit, ViewChild} from '@angular/core';
 import {ScrollDirection, ScrollEvent, WindowService} from './window.service';
 import { Subscription } from "rxjs/Subscription";
+import {Observable} from "rxjs/Rx";
 
 @Component({
 	selector: 'app-root',
 	templateUrl: './app.component.html',
 	styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit, OnDestroy {
+export class AppComponent implements OnInit, AfterViewInit, OnDestroy {
   @HostBinding('class.isOverflowing') isOverflowing = true;
 	isHeaderShown = true;
 	scrollLog = 'scroll';
 
-	@ViewChild('header') private header: ElementRef;
+	@HostBinding('style.padding-top.px') headerPadding: number;
 
-	private sub: Subscription;
+	headerOffCanvasShift = 0;
+
+	@ViewChild('header') private header: ElementRef;
+	private headerHeight: number;
+
+	private wsSub: Subscription;
+  private heightSub: Subscription;
 	constructor(private windowService: WindowService) {}
 
 	ngOnInit(): void {
-		this.sub = this.windowService.onScroll$.subscribe((e: ScrollEvent) => {
-      this.isHeaderShown = this.header.nativeElement.clientHeight > e.scrollTop || e.direction === ScrollDirection.Up;
+		this.wsSub = this.windowService.onScroll$.subscribe((e: ScrollEvent) => {
+      this.headerOffCanvasShift = this.headerHeight > e.scrollTop || e.direction === ScrollDirection.Up ? 0 : -this.headerHeight;
       this.scrollLog = `scrolling ${e.direction}`;
 		});
 	}
 
+  ngAfterViewInit(): void {
+		this.heightSub = Observable.of(this.header.nativeElement.clientHeight)
+			.subscribe(height => {
+				this.headerPadding = height + 8;
+				this.headerHeight = height
+      });
+  }
+
   ngOnDestroy(): void {
-    this.sub.unsubscribe();
+    this.wsSub.unsubscribe();
+    this.heightSub.unsubscribe();
   }
 }
