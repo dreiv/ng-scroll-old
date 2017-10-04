7b5711f031b8c43d33a724e7ac42327bfc30ceab
Andrei Voicu <Andrei-Florian.Voicu@haufe-lexware.com>
2017-10-04 16:53:56 +0300

    tinkering.

--- src/app/window.service.ts
+++ src/app/window.service.ts
@@ -4,33 +4,34 @@ import { Observable } from 'rxjs/Rx';
 export type ScrollDirection = 'up' | 'down';
 
 export const ScrollDirection = {
-	Up: 'up' as ScrollDirection,
-	Down: 'down' as ScrollDirection
+  Up: 'up' as ScrollDirection,
+  Down: 'down' as ScrollDirection
 };
 
 export class ScrollEvent {
-	constructor(public direction: ScrollDirection, public scrollTop: number) {}
+  constructor(public direction: ScrollDirection, public scrollTop: number) {
+  }
 }
 
 @Injectable()
 export class WindowService {
-	onScroll$: Observable<ScrollEvent>;
-	private previousScrollTop: number;
-	private isIOS = /iPad|iPhone/.test(navigator.userAgent);
+  onScroll$: Observable<ScrollEvent>;
+  private previousScrollTop: number;
+  private isIOS = /iPad|iPhone/.test(navigator.userAgent);
 
-	constructor() {
-		const sEl = document.scrollingElement;
-		
-		this.onScroll$ = Observable.fromEvent<UIEvent>(window, 'scroll')
-			.map((): number => this.isIOS ? Math.max(Math.min(sEl.scrollTop, sEl.scrollHeight - window.innerHeight), 0) : sEl.scrollTop)
-			.filter((currentScrollTop: number): boolean => currentScrollTop !== this.previousScrollTop)
-			.map((currentScrollTop: number): ScrollEvent => {
-				const direction = currentScrollTop > this.previousScrollTop ? ScrollDirection.Down : ScrollDirection.Up;
-				this.previousScrollTop = currentScrollTop;
+  constructor() {
+    const sEl = document.scrollingElement;
 
-				return new ScrollEvent(direction, currentScrollTop);
-			})
-			.distinctUntilChanged();
-	}
+    this.onScroll$ = Observable.fromEvent<UIEvent>(window, 'scroll')
+      .debounceTime(100)
+      .map((): number => this.isIOS ? Math.max(Math.min(sEl.scrollTop, sEl.scrollHeight - window.innerHeight), 0) : sEl.scrollTop)
+      .filter((currentScrollTop: number): boolean => currentScrollTop !== this.previousScrollTop)
+      .map((currentScrollTop: number): ScrollEvent => {
+        const direction = currentScrollTop > this.previousScrollTop ? ScrollDirection.Down : ScrollDirection.Up;
+        this.previousScrollTop = currentScrollTop;
+
+        return new ScrollEvent(direction, currentScrollTop);
+      });
+  }
 
 }
